cf48acc7c2b9f885e28f5c4c8930a098ee22baeb
shriya-1203 <ishriya12@gmail.com>
2026-03-08 03:40:43 -0500

    updated interview prep code

--- Antony/frontend/src/components/Card.jsx
+++
@@ -1,9 +0,0 @@
-import './Card.css';
-
-export default function Card({ children, className = '', hoverable = false, ...props }) {
-  return (
-    <div className={`card ${hoverable ? 'card-hoverable' : ''} ${className}`} {...props}>
-      {children}
-    </div>
-  );
-}
